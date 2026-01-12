b2c17f6a2076e56da6f4a28898461d1226905011
Juliet <albertmaverick99@gmail.com>
2026-01-12 06:57:03 +0100

    herosection

--- components/Navigation.js
+++ components/Navigation.js
@@ -19,7 +19,7 @@ export default function Navigation() {
   ]
 
   return (
-    <nav className="bg-white shadow-sm border-b border-purple-100 ">
+    <nav className="bg-white/70 shadow-sm border-b border-purple-100 ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
